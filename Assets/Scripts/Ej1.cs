5f60a21951574bfb337dbfb814153111ab21e18a
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-04-07 08:48:48 -0300

    Finished excersises 1 & 2

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    public float monto;
+    // Start is called before the first frame update
+    //2. Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible. Comunicar si la suma de los mismos supera o no el monto de dinero disponible. Luego indicar cuánto dinero sobra o falta.
+    void Start()
+    {
+        float preciosumado;
+        float resto;
+        float faltante;
+        preciosumado = precio1 + precio2 + precio3;
+        resto = monto - preciosumado;
+        faltante = preciosumado - monto;
+
+        if (preciosumado>monto)
+        {
+            Debug.Log("El monto es superado");
+            Debug.Log("Faltan " + faltante + " pesos");
+        }
+
+        else if (preciosumado<monto)
+        {
+            Debug.Log("El monto no es superado");
+            Debug.Log("Sobran " + resto + " pesos");
+        }
+
+        else
+        {
+            Debug.Log("el monto y el precio son iguales asi que no sobra ni falta dinero");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
